61f3d241140f94ce7e6289c587697d2d88b84f3b
squirrelinmyhand <mindino03@gmail.com>
2022-03-13 01:15:16 +0900

    change route => process.env.PUBLIC_URL

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import Detail from "./routes/Detail"
 function App() {
     return <Router>
         <Routes>
-            <Route path="/movie/:id" element={<Detail />}></Route>
-            <Route path="/" element={<Home />}></Route>
+            <Route path={`${process.env.PUBLIC_URL}:id`} element={<Detail />}></Route>
+            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
         </Routes>
     </Router>;
 }
